317f663f06257434d108f0180f1b37ee503bf9ce
GuiBaga16 <166162643+GuiBaga16@users.noreply.github.com>
2024-09-24 09:00:01 -0300

    atualização de alinhamento e menu

--- Sacola Fretes/menu.js
+++ Sacola Fretes/menu.js
@@ -7,24 +7,44 @@ window.addEventListener('scroll', function () {
     }
 });
 
+// Função para abrir/fechar o menu hambúrguer
 function toggleMenu() {
-    const navLinks = document.getElementById("navLinks");
-    const hamburgerIcon = document.getElementById("hamburgerIcon");
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active'); // Ativa/desativa o menu
 
-    // Alternar a exibição do menu
-    navLinks.classList.toggle("show");
-
-    // Alternar a animação do ícone de hambúrguer
-    hamburgerIcon.classList.toggle("open");
+    // Animação do ícone do hambúrguer
+    const hamburgerIcon = document.getElementById('hamburgerIcon');
+    hamburgerIcon.classList.toggle('open');
 }
 
+// Função para fechar o menu ao clicar em um link e rolar suavemente para a seção
 function closeMenu() {
-    const navLinks = document.getElementById("navLinks");
-    const hamburgerIcon = document.getElementById("hamburgerIcon");
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.remove('active'); // Fecha o menu
 
-    // Fechar o menu
-    navLinks.classList.remove("show");
-
-    // Reverter a animação do ícone de hambúrguer
-    hamburgerIcon.classList.remove("open");
+    const hamburgerIcon = document.getElementById('hamburgerIcon');
+    hamburgerIcon.classList.remove('open'); // Reseta a animação
 }
+
+// Função para remover a âncora da URL e rolar suavemente para a seção correspondente
+function smoothScroll(event) {
+    event.preventDefault(); // Previne o comportamento padrão
+    const targetId = this.getAttribute('href').substring(1); // Remove o # da href
+    const targetElement = document.getElementById(targetId);
+
+    // Rola suavemente até o elemento
+    targetElement.scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // Remove o # da URL imediatamente
+    history.pushState(null, null, ' ');
+}
+
+// Adiciona o comportamento de rolagem suave e remoção da âncora ao carregar a página
+window.onload = function () {
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+};
