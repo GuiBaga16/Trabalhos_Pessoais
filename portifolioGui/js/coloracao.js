2e83b1b59f95b4d02680a046656b0d981af96cc4
GuiBaga16 <166162643+GuiBaga16@users.noreply.github.com>
2024-11-07 11:28:50 -0300

    update7.11

--- portifolioGui/js/coloracao.js
+++ portifolioGui/js/coloracao.js
@@ -5,3 +5,16 @@ trilho.addEventListener('click', ()=>{
   trilho.classList.toggle('dark')
   body.classList.toggle('dark')
 });
+
+function toggleDarkMode() {
+  const body = document.getElementById('pageBody');
+  const aboutSection = document.querySelector('.about-section');
+  const projectsSection = document.querySelector('.projects-section');
+  const contactSection = document.querySelector('.contact-section');
+
+  // Alternar classes de modo escuro
+  body.classList.toggle('dark');
+  aboutSection.classList.toggle('dark');
+  projectsSection.classList.toggle('dark');
+  contactSection.classList.toggle('dark');
+}
